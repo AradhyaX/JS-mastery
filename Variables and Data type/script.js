b658045d84d632c415cc04d32c9fd37fc2a853c5
Aradhya <soularadhya@gmail.com>
2026-02-02 13:24:11 +0530

    Add Variables and Data type JavaScript logic

---
+++ Variables and Data type/script.js
@@ -0,0 +1,17 @@
+//  var variableName = 'Welcome to earth';
+
+//  console.log(variableName);
+
+// let and const are also used to create a variable
+
+// let variableName = 'Welcome to earth';
+
+// console.log(variableName);
+
+// variableName = 'hello';
+
+// console.log(variableName);
+
+const variableName ='hello';
+
+console.log(variableName);
