637b79d8f295779d474cf93bb53ca93d3091aa78
Aradhya <soularadhya@gmail.com>
2026-02-03 02:26:40 +0530

    Update Variables and Data type script: add examples for Strings, Numbers, Booleans, Objects, and typing

--- Variables and Data type/script.js
+++ Variables and Data type/script.js
@@ -12,6 +12,96 @@
 
 // console.log(variableName);
 
-const variableName ='hello';
+// const variableName ='hello';
 
-console.log(variableName);
+// console.log(variableName);
+
+
+//STRING
+
+// const singleQuoates = 'Hello!';
+// const doubleQuoates = 'Hello!!';
+// const backTicks = `Hello!!!`;
+
+// console.log(singleQuoates);
+// console.log(doubleQuoates);
+// console.log(backTicks);
+
+// const name ='jane';
+// const backTicks = `Hello, ${name}`;
+
+// console.log(backTicks);
+
+
+//NUMBERS
+
+/* const wholeNumber = 555;
+ const decimalNumber = 0.5;
+
+ console.log(wholeNumber);
+ console.log(decimalNumber);
+
+const string = 'hello';
+const secondNumber = 10;
+
+const result = string / secondNumber;
+console.log(result);*/
+// (nan - not a number)
+
+
+//BOOLEANS
+
+// true
+//false
+
+// const isCool =  true;
+
+// if(isCool){
+//   console.log("hi man, you're cool")
+// }else{
+//   console.log('oh,hi......');
+// }
+
+//NULL
+    // null means is unknown or empty
+
+
+ //UNDEFINED
+
+     // meaning of undifined is its value is not assigned
+
+// let x;
+// console.log(x);
+
+
+//OBJECTS
+
+// const person = {
+//   name:'john',
+//   age: 25,
+// }
+    // Dot notation
+// console.log(person.name);
+
+    //Array
+// const arr = [1,2,3,4]
+// console.log(arr);
+
+
+// const date =  Date();
+// console.log(date);
+
+
+
+//Statically typed
+let message ='Hello, World';
+console.log(message);
+
+message = 5;
+console.log(message);
+//Dynamically typed
+
+// JavaScript is dynamically typed
+
+
+
