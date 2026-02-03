7e9a9b6e48990b35b38b8a02082f15c25bb7a423
Aradhya <soularadhya@gmail.com>
2026-02-03 17:54:37 +0530

    Add 03_OPERATORS and update variables script

---
+++ 03_OPERATORS/script.js
@@ -0,0 +1,79 @@
+// const a = 5;
+// const b = 10;
+
+// let result = 0;
+
+// //Addition
+// result = a + b;
+// console.log(result);
+
+
+// COMPARISON OPERATOR ⇒ true/false
+/*
+const a = 5;
+const b = 10;
+
+// Greater than
+console.log(a > b);
+
+// Greater than or equal to
+console.log(a >= b);
+
+// Less than
+console.log(a < b);
+
+// Less than or equal to
+console.log(a <= b);
+
+// Is equal
+console.log(a == b);
+
+// Not equal
+console.log(a != b);
+
+// Strict Eqaulity
+console.log(a === b);
+
+// Strict Ineqaulity
+console.log(a !== b);
+
+
+// STRICT EQUALITY
+// Compares VALUES and DATA TYPES
+// return  true only if both are the same
+
+console.log(20 === '20');  //False
+
+
+
+// LOOSE EQUALITY
+// Doesn't compare DATA TYPES
+
+console.log(20 == "20");  //true
+*/
+
+/*
+// LOGICAL OPERATOR
+
+// AND && ⇒ ALL OPERANDS ARE TRUE ⇒ TRUE
+console.log(true && false);  //false
+console.log(true && true && true);  //true
+
+
+// OR || ⇒ AT LEAST ONE OPERANDS IS TRUE ⇒ true
+console.log(true || false); // true
+console.log(false || false); //false
+
+
+// NOT ! ⇒ IT REVERSES BOOLEAN VALUE
+console.log(!true); //false
+console.log(!false); //true  */
+
+
+// ASSIGNMENT OPERATOR
+let number = 5;
+
+number += 5;
+// number = number + 5;
+
+console.log(number);
