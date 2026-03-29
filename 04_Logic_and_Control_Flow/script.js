e510bec90f7b13fa6d63ccc5c342ffd87de149fe
Aradhya <soularadhya@gmail.com>
2026-03-29 23:38:14 +0530

    Update logic and control flow script

--- 04_Logic_and_Control_Flow/script.js
+++ 04_Logic_and_Control_Flow/script.js
@@ -11,7 +11,7 @@
  */
 
 
-        // TRUTHY-FALSY VALUES
+                //! TRUTHY-FALSY VALUES
 
   // if (0){
   //    console.log('IN HERE');
@@ -27,16 +27,16 @@
 // undefined
 // Nan
 
-      //LOGICAL OPERATOR
+                //! LOGICAL OPERATOR
 
       // const age = 19;
       // const isCool = true;
 
       // is cool and is above the age of 18
 
-      console.log ('truthy' && 1 && 'test' && 999); //999
+    //   console.log ('truthy' && 1 && 'test' && 999); //999
         
-      console.log('truthy' || 1  ||'test' || 999);
+    //   console.log('truthy' || 1  ||'test' || 999);
 
       // JavaScript is lazy language in AND OPERATOR it returns the first false value and if there is no false value it returns the last true value
 
@@ -45,4 +45,75 @@
       // In NOT operator it just gives opposite value
 
 
-               // SWITCH STATEMENT    
+               //! SWITCH STATEMENT   
+    
+    // const superhero = 'Iron Man';
+
+    // switch(superhero) {
+    //     case 'Iron Man':
+    //         console.log('I am Iron man..');
+    //         break;
+
+    //     case 'Thor':
+    //         console.log('That is my hammer!');
+    //         break;
+
+    //     case 'Captain America':
+    //         console.log('Never give up');
+    //         break;
+        
+    //     case 'Black Widow':
+    //         console.log('One shot, one kill!');
+    //         break;
+    //     default:
+    //         console.log('Enter a valid superhero name'); 
+    // }
+
+             //! TERNARY OPERATOR
+    
+    if (condition) {
+        // block of code if true
+    } else {
+        // block of code if false
+    }
+     
+     //* condition ? block of code if true : block of code if false
+
+     //* condition ? true : false
+
+     const age = 18;
+     if (age >= 18) {
+        console.log('You can drive.');
+     } else {
+        console.log('You can not drive yet.')
+     }
+
+    age >= 18 ? console.log('You can drive.') : console.log('You can not drive yet.')
+
+
+
+                  //! LOOPING WHILE AND FOR-LOOP
+
+    //?  The "while" loop
+
+    while (condition) {
+        //code
+    }
+
+    let i = 0 ;
+    while (i < 10) {
+        console.log(i);
+        i++;
+    }
+
+
+    //? The "for" loop
+
+    for([initialization];[condition]; [final-expression]){
+
+    }
+
+    for(let i = 0; i < 10; i++){
+        console.log(i);
+
+    }
